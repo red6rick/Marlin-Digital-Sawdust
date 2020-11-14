bd146ffe3e411f9e82054c29ca70ccff2346cfa4
Rick VanNorman <rick@digital-sawdust.com>
2020-11-14 14:27:29 -0600

    Branch for ELF; corexy enabled

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(rcvn, frankenz config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(rcvn, elf config)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "FrankenZ"
+#define CUSTOM_MACHINE_NAME "ELF"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -608,7 +608,7 @@
 
 // Enable one of the options below for CoreXY, CoreXZ, or CoreYZ kinematics,
 // either in the usual order or reversed
-//#define COREXY
+#define COREXY
 //#define COREXZ
 //#define COREYZ
 //#define COREYX
